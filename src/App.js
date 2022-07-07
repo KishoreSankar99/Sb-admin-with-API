d9eafa256d1c3de761495944e2a50a6ac7c16ead
KishoreSankar99 <kishorecruise99@gmail.com>
2022-07-07 18:57:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dashboard from "./Dashboard";
+import Sidebar from "./Sidebar";
+import Topbar from "./Topbar";
 
 function App() {
+  let data={
+    earningsMonthly:'40,000',
+    earningsAnnual:'215,000',
+    tasks:40,
+    pendingRequests:18
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <Sidebar />
+      <div id="content-wrapper" class="d-flex flex-column">
+        <div id="content">
+          <Topbar />
+          <div class="container-fluid">
+            <div class="d-sm-flex align-items-center justify-content-between mb-4">
+              <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+              <a href="#"
+                class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+                <i class="fas fa-download fa-sm text-white-50"></i> Generate
+                Report
+              </a>
+            </div>
+            <div class="row">
+            <Dashboard data={data}/>
+           
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
